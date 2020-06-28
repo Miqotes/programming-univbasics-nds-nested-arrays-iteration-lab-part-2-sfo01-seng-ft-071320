e73aab8b24ce98e317afe911002ccb7bb8adb1a0
Ashlee Barksdale <ashleelennab@gmail.com>
2020-06-28 23:14:53 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,7 +2,7 @@ def find_min_in_nested_arrays(src)
   row_index = 0 
   min_results = []
   while row_index < src.count do 
-    min_reults << src[row_index].min
+    min_results << src[row_index].min
     row_index += 1
   end
   min_results
